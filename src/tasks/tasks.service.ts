b1bd8940964e32c57d97f07f94b031c1aca97b22
mkkravchenkoo <mkkravchenkoo@gmail.com>
2020-10-19 22:09:37 +0300

    Not Found 404

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
 import {Task, TaskStatuses} from "./tasks.model";
 import { v4 as uuidv4 } from 'uuid';
 import {CreateTaskDto} from "./dto/create-task-dto";
@@ -49,7 +49,13 @@ export class TasksService {
     }
 
     getTaskById(id:string):Task{
-        return this.tasks.find((task) => task.id===id)
+        const found = this.tasks.find((task) => task.id===id);
+
+        if(!found){
+            throw new NotFoundException("Task not found");
+        }
+
+        return found
     }
 
     updateTaskStatus(id:string, status:TaskStatuses):Task{
