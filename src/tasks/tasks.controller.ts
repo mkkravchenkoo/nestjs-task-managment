fee39a34749d27392c38f331baea6b1df32ce3bc
mkkravchenkoo <mkkravchenkoo@gmail.com>
2020-10-25 14:49:48 +0200

    update task status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -16,6 +16,7 @@ import {CreateTaskDto} from "./dto/create-task-dto";
 import {GetTaskFilterDto} from "./dto/get-task-filter-dto";
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 import {Task} from "./task.entity";
+import {TaskStatus} from "./task-status.enum";
 
 
 @Controller('tasks')
@@ -40,18 +41,16 @@ export class TasksController {
     deleteTask(@Param("id", ParseIntPipe) id:number):Promise<void>{
         return this.tasksService.deleteTask(id);
     }
-    //
-    // @Patch('/:id/status')
-    // updateTaskStatus(
-    //     @Param("id") id:string,
-    //     @Body("status", TaskStatusValidationPipe) status:TaskStatuses
-    // ):Task{
-    //     return this.tasksService.updateTaskStatus(id, status)
-    // }
-    //
     @Post()
     @UsePipes(ValidationPipe)
     createTask(@Body() createTaskDto:CreateTaskDto):Promise<Task>{
         return this.tasksService.createTask(createTaskDto)
+    }
+    @Patch('/:id/status')
+    updateTaskStatus(
+        @Param("id", ParseIntPipe) id:number,
+        @Body("status", TaskStatusValidationPipe) status:TaskStatus
+    ):Promise<Task>{
+        return this.tasksService.updateTaskStatus(id, status)
     }
 }
