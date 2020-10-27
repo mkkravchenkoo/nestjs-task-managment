b587cb9fc13683d232a88408494fbf4b7738a375
mkkravchenkoo <mkkravchenkoo@gmail.com>
2020-10-27 20:20:00 +0200

    sign in (check password)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,4 +10,9 @@ export class AuthController {
     signUp(@Body(ValidationPipe) authCredentialsDto:AuthCredentialsDto):Promise<void>{
        return this.authService.signUp(authCredentialsDto);
     }
+
+    @Post("/signin")
+    singIn(@Body(ValidationPipe) authCredentialsDto:AuthCredentialsDto):Promise<void>{
+        return this.authService.signIn(authCredentialsDto);
+    }
 }
