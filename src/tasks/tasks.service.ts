50ca148f123f4a44758ecf190c5c51aa78404525
mkkravchenkoo <mkkravchenkoo@gmail.com>
2020-10-28 20:35:54 +0200

    user create task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -5,6 +5,7 @@ import {InjectRepository} from "@nestjs/typeorm";
 import {Task} from "./task.entity";
 import {TaskStatus} from "./task-status.enum";
 import {GetTaskFilterDto} from "./dto/get-task-filter-dto";
+import {User} from "../auth/user.entity";
 
 @Injectable()
 export class TasksService {
@@ -19,8 +20,8 @@ export class TasksService {
 
     }
 
-    async createTask(createTaskDto:CreateTaskDto):Promise<Task>{
-        return this.taskRepository.createTask(createTaskDto)
+    async createTask(createTaskDto:CreateTaskDto, user:User):Promise<Task>{
+        return this.taskRepository.createTask(createTaskDto, user)
     }
 
     async deleteTask(id:number):Promise<void>{
